5cd671935a3c6c8eaff1ef6344d200dac47f45fd
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-01-06 14:56:45 +0100

    example exhenced

--- fileSystem/writeFile.js
+++ fileSystem/writeFile.js
@@ -4,7 +4,33 @@ const fs = require('fs').promises;
 
 const PATH = "test.txt"
 
-async function writeFile() {
+let ItemApple = {
+    name: "Apfel",
+    menge: "60",
+    preis: "500$",
+}
+
+let ItemLemon = {
+    name: "Lemon",
+    menge: "200",
+    preis: "50$",
+}
+
+let ItemMandarine = {
+    name: "Mandarine",
+    menge: "15",
+    preis: "40$",
+}
+
+let ItemBanane = {
+    name: "Banane",
+    menge: "65",
+    preis: "70$",
+}
+
+const items = [ItemApple, ItemLemon, ItemMandarine, ItemBanane];
+
+async function writeFile(callBackFunction2, ItemsObject) {
     try{
 
         const Content = "Name, Anzahl, Preis";
@@ -12,6 +38,10 @@ async function writeFile() {
         const write = () => {
             fs.writeFile(PATH, Content, { encoding: "utf8" }).then(() => {
                 console.log('Title gesetzt');
+            }).then(() => {
+                ItemsObject.forEach(ItemObject => {
+                    callBackFunction2(ItemObject.name, ItemObject.anzahl, ItemObject.preis);
+                })
             });
         }
         write();
@@ -38,11 +68,23 @@ async function addItem(name, anzahl, preis) {
     }
 }
 
-(async function() {
-    await writeFile();
-    await addItem("Apfel", "25", "20$");
-    await addItem("Banane", "65", "15$");
-    await addItem("Kirsche", "45", "30$");
-    await addItem("Kaki", "15", "40$");
-    await addItem("Birne", "35", "25$");
-})();
+// (async function() {
+//     await writeFile();
+//     await addItem("Apfel", "25", "20$");
+//     await addItem("Banane", "65", "15$");
+//     await addItem("Kirsche", "45", "30$");
+//     await addItem("Kaki", "15", "40$");
+//     await addItem("Birne", "35", "25$");
+// })();
+
+
+function addToList(callBackFunction) {
+
+    const setList = () => {
+        callBackFunction(addItem, items);
+    }
+
+    setList();
+}
+
+addToList(writeFile)
