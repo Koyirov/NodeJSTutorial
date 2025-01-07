a5fe768310d996d07687389832ea597aac784f5e
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-01-07 14:37:55 +0100

    error handling

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -2,11 +2,27 @@
 import { server, PORT } from "./server";
 
 server.get('/', function (req, res) {
-    res.send('Hello World')
+    res.json({
+        name: "Max",
+        alter: 36,
+        geburtsort: "USA",
+        besitz: {
+            auto: "VW-Golg",
+            buch: "Bla"
+        }
+    });
 })
 
 server.post('/profil', function (req, res) {
     res.send('Moinsen User')
+})
+
+
+// 404 error handling
+server.use((req, res, next) => {
+    res.status(404).json({
+        status: "Seite existiert nicht! 404 Error"
+    });
 })
 
 server.listen(PORT, () => console.log('Server ist gestartet auf Port  ' + PORT));
