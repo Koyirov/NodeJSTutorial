367b9a67c41a68c066676214099f114e6cde99d3
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-01-07 13:32:38 +0100

    server expanded

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -5,4 +5,8 @@ server.get('/', function (req, res) {
     res.send('Hello World')
 })
 
+server.get('/profil', function (req, res) {
+    res.send('Moinsen User')
+})
+
 server.listen(PORT, () => console.log('Server ist gestartet auf Port  ' + PORT));
