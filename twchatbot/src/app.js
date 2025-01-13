3426416490c895e4f4316761397ace1ef535602a
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-01-13 08:34:26 +0100

    client connected

--- twchatbot/src/app.js
+++ twchatbot/src/app.js
@@ -1,1 +1,22 @@
-console.log("App geladen...");
+import {MAIN_CLIENT} from "./clients";
+
+MAIN_CLIENT.connect();
+
+MAIN_CLIENT.on("message", (channel, userstate, message, self) => {
+
+    if(self) return;
+
+    switch(userstate["message-type"]) {
+        case "action":
+            // Do something
+            break;
+        case "chat":
+            console.log(message);
+            break;
+        case "whisper":
+            // Do something
+            break;
+        default:
+            break;
+    }
+})
